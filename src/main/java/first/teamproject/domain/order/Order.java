6e8ba5961a5fe11e446d032ae2e0b396223b76d7
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2023-02-08 15:50:01 +0900

    Add files via upload

--- src/main/java/first/teamproject/domain/order/Order.java
+++ src/main/java/first/teamproject/domain/order/Order.java
@@ -1,5 +1,5 @@
 package first.teamproject.domain.order;
-
+import first.teamproject.domain.cart.Cart;
 import first.teamproject.domain.member.Member;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,28 +8,45 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+
+ 주문 관련 클래스
+ */
 @Getter
 @Setter
 public class Order {
 
-    private Long orderNo;
-    private Member member;
-    private List<OrderItem> orderItems = new ArrayList<>();
-    private Delivery delivery;
-    private LocalDateTime orderDate;
-    private OrderStatus status;
+    private Long orderNo; // 주문 번호
+    private Member member; // 주문 회원
+    private List<OrderItem> orderItems = new ArrayList<>(); // 주문 상품 목록
+    private Delivery delivery; // 배송 정보
+    private LocalDateTime orderDate; // 주문 날짜
+    private OrderStatus status; // 주문 상태
 
+    private Cart cart;
+
+    /**
+
+     기본 생성자 (외부에서 호출 불가)
+     */
     private Order() {
     }
+    /**
 
-    public Order(Long orderNo, Member member, Delivery delivery, LocalDateTime orderDate, OrderStatus status) {
-        this.orderNo = orderNo;
+     주문 객체 생성자
+     @param orderNo 주문 번호
+     @param member 주문 회원
+     @param delivery 배송 정보
+     @param orderDate 주문 날짜
+     @param status 주문 상태
+     */
+    // 주문 객체 생성 메서드
+    public Order(Member member, Delivery delivery, Cart cart){
         this.member = member;
         this.delivery = delivery;
-        this.orderDate = orderDate;
-        this.status = status;
+        this.cart = cart;
+        this.orderDate = LocalDateTime.now();
     }
-
     @Override
     public String toString() {
         return "Order{" +
@@ -42,45 +59,32 @@ public class Order {
                 '}';
     }
 
+    /**
+
+     주문 상품 추가
+     @param orderItem 주문 상품
+     */
     public void addOrderItem(OrderItem orderItem){
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
+/**
 
-    public static Order createOrder(Member member,Delivery delivery,OrderItem...orderItems){
-        Order order =new Order();
-        order.setMember(member);
-        order.setDelivery(delivery);
-        for (OrderItem orderItem : orderItems) {
-            order.addOrderItem(orderItem);
-        }
-        order.setStatus(OrderStatus.ORDER);
-        order.setOrderDate(LocalDateTime.now());
-        return order;
+ 주문 객체 생성 메서드
+ @param member 주문 회원
+ @param delivery 배송 정보
+ @param orderItems 주문 항목 목록
+ @return 생성된 주문 객체
+ */
+public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+    Order order = new Order();
+    order.setMember(member);
+    order.setDelivery(delivery);
+    for (OrderItem orderItem : orderItems) {
+        order.addOrderItem(orderItem);
     }
-    /**
-     * 비즈니스로직
-     */
-    /*주문 취소*/
-    public void cancel(){
-        if(delivery.getStatus()==DeliveryStatus.COMP){
-            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
-        }
-        this.setStatus(OrderStatus.CANCEL);
-        for (OrderItem orderItem : orderItems) {
-            orderItem.cancel();
-        }
-    }
-
-    /**
-     * 조회로직
-     */
-    /*전체 주문 가격 조회*/
-    public int getTotalPrice(){
-        int totalPrice=0;
-        for (OrderItem orderItem : orderItems) {
-            totalPrice += orderItem.getTotalPrice();
-        }
-        return totalPrice;
-    }
+    order.setStatus(OrderStatus.ORDER);
+    order.setOrderDate(LocalDateTime.now());
+    return order;
+}
 }
